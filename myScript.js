2e459c90b61fdce18d7b136c7c8d55d4ffef7f23
TeyTang <73002000+TeyTang@users.noreply.github.com>
2024-06-14 09:19:45 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -18,7 +18,7 @@ document.getElementById("content").style="margin-left:0px";
 if(document.getElementById("mySidebar").style.display == "none"){
 document.getElementById("mySidebar").style.display = "block";
 document.getElementById("content").style="margin-left:200px";
- document.getElementById("content").style="margin-right:00px";
+ document.getElementById("content").style.width+=200px;
 
 }
  
@@ -27,7 +27,7 @@ document.getElementById("content").style="margin-left:200px";
 function closeSidebar() {
 document.getElementById("mySidebar").style.display = "none";
 document.getElementById("content").style="margin-left:0px";
- document.getElementById("content").style="margin-right:00px";
+ document.getElementById("content").style.width+=200px;
 
 
 }
